3fcdb5ca80d8e27ad6cb0b6a215d1c86614293b5
AlanNabiev <alannabiev2001@gmail.com>
2021-10-29 20:25:05 +0300

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1 class="text-red-500 font-bold">CryptoGBoors website</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App"
+});
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  /* color: #2c3e50; */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
